594e6e03da4bab3dc78c18373364815058dd5f2a
aishwaryaaish12 <aishwaryamanikandan035@gmail.com>
2024-03-06 18:40:26 +0530

    latest commit

--- src/Section/Services/ServicesForm.js
+++ src/Section/Services/ServicesForm.js
@@ -6,6 +6,7 @@ function ServicesForm (){
   return(
     <Box sx={{ display: 'flex',justifyContent:'space-around',gap: '3', backgroundColor: '#ABCDCD',flexDirection:'column'}}>
 <Box  sx={{fontSize:50,textAlign:'center',marginTop:-50}}>Our Services</Box>
+<Box sx={{display:'Flex'}}></Box>
       </Box>
 
       
