9682bb6b1f5badb24f5e7224ded5c7d56fe29a08
aishwaryaaish12 <aishwaryamanikandan035@gmail.com>
2024-03-06 18:46:49 +0530

    new service page

--- src/Section/Services/ServicesForm.js
+++ src/Section/Services/ServicesForm.js
@@ -1,16 +1,72 @@
 import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
 
-function ServicesForm (){
-  return(
-    <Box sx={{ display: 'flex',justifyContent:'space-around',gap: '3', backgroundColor: '#ABCDCD',flexDirection:'column'}}>
-<Box  sx={{fontSize:50,textAlign:'center',marginTop:-50}}>Our Services</Box>
-<Box sx={{display:'Flex'}}></Box>
-      </Box>
+function ServicesForm() {
+  return (
+    <Box>
+    <Box>
+    <Typography sx={{fontSize:50,textAlign:'center',marginBottom:5}}>Our Services</Typography>
 
-      
+    </Box>
+    <Box sx={{ display: 'flex', gap: '28px' }}>
+
+      <Card sx={{ maxWidth: 305 }}>
+        <CardMedia
+          sx={{ height: 180 }}
+          image="/Assets/Crossplatformposting.png"
+    
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" sx={{textAlign:'center'}}>
+            Cross-Platform Posting
+          </Typography>
+          <Typography variant="body2" color="text.secondary" sx={{textAlign:'center'}}>
+          Support posting on multiple social media platforms
+ simultaneously, saving time for users managing 
+accounts on various channels.</Typography>
+        </CardContent>
+      </Card>
+
+      <Card sx={{ maxWidth: 305 }}>
+        <CardMedia
+          sx={{ height: 180 }}
+          image="/Assets/ContentCalender.png"
+        
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" sx={{textAlign:'center'}}>
+            Content Calender
+          </Typography>
+          <Typography variant="body2" color="text.secondary" sx={{textAlign:'center'}}>
+          Content calendar that provides a visual overview 
+of scheduled posts, making it easier to plan and 
+organize content across different platforms.          </Typography>
+        </CardContent>
+      </Card>
+      <Card sx={{ maxWidth: 305 }}>
+        <CardMedia
+          sx={{ height: 180 }}
+          image="/Assets/Emailandwhatsappcampaign.png"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" sx={{textAlign:'center'}}>
+             Campaign
+          </Typography>
+          <Typography variant="body2" color="text.secondary" sx={{textAlign:'center'}}>
+          Support users in executing comprehensive outreach plans by incorporating features for both email and WhatsApp campaigns on the platform. 
+                     </Typography>
+        </CardContent>
+      </Card>
+
+    </Box>
+    </Box>
   );
 }
 
-  export default ServicesForm
+export default ServicesForm;
